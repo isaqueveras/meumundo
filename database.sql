e11677dd8548abfe3e437671568bb5b4c75e0f8b
Isaque Veras <isaqueveras@alu.ufc.br>
2023-07-30 11:54:20 -0300

    feat: add database

--- database.sql
+++ database.sql
@@ -1,10 +1,24 @@
 CREATE DATABASE IF NOT EXISTS "nossobr";
 USE "nossobr";
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE t_estados (
+	id VARCHAR(8) PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  sigla VARCHAR(2) NOT NULL
+);
 
-CREATE TABLE t_cities (
-	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE t_cidades (
+	id VARCHAR(8) PRIMARY KEY,
+	estado_id VARCHAR(8) NOT NULL REFERENCES t_estados (id),
   city VARCHAR(100) NOT NULL,
-  "state" VARCHAR(2) NOT NULL
+  slug VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE t_artigos (
+	id VARCHAR(8) PRIMARY KEY,
+  conteudo TEXT,
+  info JSON NOT NULL,
+	cidade_id VARCHAR(8) NOT NULL REFERENCES t_cidades (id),
+	criacao TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	atualizacao TIMESTAMPTZ
 );
