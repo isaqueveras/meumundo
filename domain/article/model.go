4284b781eca7f61fb74371893d4d3163349678b8
Isaque Veras <isaqueveras@alu.ufc.br>
2023-09-08 21:30:22 -0300

    feat: add sections in content

--- domain/article/model.go
+++ domain/article/model.go
@@ -9,9 +9,16 @@ import (
 type Article struct {
 	ID          *string           `json:"id,omitempty"`
 	CityID      *string           `json:"city_id,omitempty"`
-	Content     *string           `json:"content,omitempty"`
+	Content     *[]*Section       `json:"content,omitempty"`
 	Children    *[]*people.People `json:"children,omitempty"`
 	BorderTowns *[][3]*string     `json:"border_towns,omitempty"`
 	CreatedAt   *time.Time        `json:"created_at,omitempty"`
 	UpdatedAt   *time.Time        `json:"updated_at,omitempty"`
+}
+
+// Section ...
+type Section struct {
+	Type  *string `json:"type,omitempty"`
+	Title *string `json:"title,omitempty"`
+	Text  *string `json:"text,omitempty"`
 }
