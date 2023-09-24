213242fe8d2bd556c505d04b35f5691fc8c6c841
Isaque Veras <isaqueveras@alu.ufc.br>
2023-09-23 21:20:10 -0300

    feat: add table t_attractions

--- database.sql
+++ database.sql
@@ -78,7 +78,28 @@ CREATE TABLE t_address (
 	article_id VARCHAR(8) REFERENCES t_article (id),
 	"name" VARCHAR(150) NOT NULL,
 	"number" VARCHAR,
+	complement VARCHAR(150),
 	zip_code INTEGER,
 	latitude NUMERIC NOT NULL,
 	longitude NUMERIC NOT NULL
+);
+
+CREATE TABLE t_attractions (
+	id VARCHAR(8) PRIMARY KEY,
+	city_id VARCHAR(8) NOT NULL REFERENCES t_cities (id),
+ 	article_id VARCHAR(8) REFERENCES t_article (id),
+	"name" VARCHAR NOT NULL,
+	"short_desc" VARCHAR(150) NOT NULL,
+	activity TEXT[] NOT NULL,
+	"site" VARCHAR(100),
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE t_attractions_props (
+	id VARCHAR(8) PRIMARY KEY,
+	attractions_id VARCHAR(8) NOT NULL REFERENCES t_attractions (id),
+	"name" VARCHAR(30) NOT NULL,
+	"value" VARCHAR(150) NOT NULL,
+	sortkey INTEGER NOT NULL
 );
